70c27d10f8c3df933f6d86868f30e70b5387fd12
Ana-Maria-C <95433627+Ana-Maria-C@users.noreply.github.com>
2023-04-09 23:42:01 +0300

    +....

--- stats-script.js
+++ stats-script.js
@@ -1,15 +1,15 @@
-
 // console.log("year: " + year.innerHTML);
-const year = document.getElementById("year").textContent = localStorage.getItem("textvalue");
+const year = document.getElementById("year");
+if (localStorage.getItem("textvalue")) {
+  year.innerHTML = localStorage.getItem("textvalue");
+}
 console.log("text " + text);
 // afisare grafic/tabel
 function displayImage() {
-    let selectedOption = document.getElementById("select-options").value;
-    if (selectedOption === 'Grafic') {
-        document.getElementById("display-image").src = "./info/grafic.png";
-    } else {
-        document.getElementById("display-image").src = "./info/tabel.png";
-    }
-    
+  let selectedOption = document.getElementById("select-options").value;
+  if (selectedOption === "Grafic") {
+    document.getElementById("display-image").src = "./info/grafic.png";
+  } else {
+    document.getElementById("display-image").src = "./info/tabel.png";
+  }
 }
-
