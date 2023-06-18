50b41bc48090898cf55afb9022e4f9b1c774ab18
Ana-Maria-C <95433627+Ana-Maria-C@users.noreply.github.com>
2023-06-18 19:14:44 +0300

    + grafic + pie chart

--- src/static/stats-script.js
+++ src/static/stats-script.js
@@ -4,12 +4,79 @@ if (localStorage.getItem("textvalue")) {
   year.innerHTML = localStorage.getItem("textvalue");
 }
 console.log("text " + text);
-// afisare grafic/tabel
+// afisare grafic/tabel/pie chart
 function displayImage() {
   let selectedOption = document.getElementById("select-options").value;
+  let content = document.querySelector(".content");
+
   if (selectedOption === "Grafic") {
-    document.getElementById("display-image").src = "../static/info/grafic.png";
-  } else {
-    document.getElementById("display-image").src = "../static/info/tabel.png";
+    const dataValues = [10, 20, 30, 40, 50];
+    const labels = ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"];
+    const colors = ["#ff0000", "#36a2eb", "#ffce56", "#ff0080", "#676266"];
+    drawCustomGrafic(dataValues, labels, colors);
+  } else if (selectedOption === "Tabel") {
+    content.innerHTML =
+      '<img id="display-image" src="../static/info/tabel.png" alt="info" />';
+  } else if (selectedOption === "Pie Chart") {
+    // Exemplu de utilizare
+    const dataValues = [15, 20, 30, 17, 8, 7];
+    const colors = [
+      "#ff0000",
+      "#36a2eb",
+      "#ffce56",
+      "#ff0080",
+      "#676266",
+      "#709430",
+    ];
+    const totalValue = 100;
+    drawPieChart(dataValues, colors, totalValue);
   }
+}
+
+function drawPieChart(dataValues, colors, totalValue) {
+  document.querySelector(".content").innerHTML = "";
+  const canvas = document.createElement("canvas");
+  canvas.id = "chartCanvas";
+  document.querySelector(".content").innerHTML = "";
+  document.querySelector(".content").appendChild(canvas);
+
+  const ctx = canvas.getContext("2d");
+  const data = {
+    labels: ["Red", "Blue", "Yellow", "Pink", "Gray", "Green"],
+    datasets: [
+      {
+        data: dataValues,
+        backgroundColor: colors,
+      },
+    ],
+  };
+
+  new Chart(ctx, {
+    type: "pie",
+    data: data,
+  });
+}
+
+function drawCustomGrafic(dataValues, labels, colors) {
+  document.querySelector(".content").innerHTML = "";
+  const canvas = document.createElement("canvas");
+  canvas.id = "chartCanvas";
+  document.querySelector(".content").innerHTML = "";
+  document.querySelector(".content").appendChild(canvas);
+
+  const ctx = canvas.getContext("2d");
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        data: dataValues,
+        backgroundColor: colors,
+      },
+    ],
+  };
+
+  new Chart(ctx, {
+    type: "bar", // Aici poți schimba tipul de grafic: bar, line, etc.
+    data: data,
+  });
 }
