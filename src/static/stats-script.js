0fd4734bd7b8513fdd667d7cda411eeee8eb44a5
Ana-Maria-C <95433627+Ana-Maria-C@users.noreply.github.com>
2023-06-19 14:35:18 +0300

    + reprezentare in 3 moduri

--- src/static/stats-script.js
+++ src/static/stats-script.js
@@ -3,20 +3,23 @@ const year = document.getElementById("year");
 if (localStorage.getItem("textvalue")) {
   year.innerHTML = localStorage.getItem("textvalue");
 }
-console.log("text " + text);
+//console.log("text " + text);
 // afisare grafic/tabel/pie chart
 function displayImage() {
   let selectedOption = document.getElementById("select-options").value;
   let content = document.querySelector(".content");
 
-  if (selectedOption === "Grafic") {
+  if (selectedOption === "Bar Chart") {
     const dataValues = [10, 20, 30, 40, 50];
     const labels = ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"];
     const colors = ["#ff0000", "#36a2eb", "#ffce56", "#ff0080", "#676266"];
     drawCustomGrafic(dataValues, labels, colors);
-  } else if (selectedOption === "Tabel") {
-    content.innerHTML =
-      '<img id="display-image" src="../static/info/tabel.png" alt="info" />';
+  } else if (selectedOption === "Bubble Chart") {
+    const dataValues = [10, 20, 30, 40, 50];
+    const labels = ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"];
+    const sizes = [5, 10, 15, 20, 25];
+    const colors = ["#ff0000", "#36a2eb", "#ffce56", "#ff0080", "#676266"];
+    drawBubbleChart(dataValues, labels, sizes, colors);
   } else if (selectedOption === "Pie Chart") {
     // Exemplu de utilizare
     const dataValues = [15, 20, 30, 17, 8, 7];
@@ -76,7 +79,47 @@ function drawCustomGrafic(dataValues, labels, colors) {
   };
 
   new Chart(ctx, {
-    type: "bar", // Aici poți schimba tipul de grafic: bar, line, etc.
+    type: "bar", // Aici pot schimba tipul de grafic: bar, line, etc.
     data: data,
+  });
+}
+
+function drawBubbleChart(dataValues, labels, sizes, colors) {
+  document.querySelector(".content").innerHTML = "";
+  const canvas = document.createElement("canvas");
+  canvas.id = "chartCanvas";
+  document.querySelector(".content").innerHTML = "";
+  document.querySelector(".content").appendChild(canvas);
+
+  const ctx = canvas.getContext("2d");
+  const data = {
+    datasets: [
+      {
+        label: "Data",
+        data: dataValues.map((value, index) => ({
+          x: value,
+          y: index + 1,
+          r: sizes[index],
+        })),
+        backgroundColor: colors,
+      },
+    ],
+  };
+
+  new Chart(ctx, {
+    type: "bubble",
+    data: data,
+    options: {
+      scales: {
+        y: {
+          ticks: {
+            stepSize: 1,
+            callback: function (value, index) {
+              return labels[index];
+            },
+          },
+        },
+      },
+    },
   });
 }
